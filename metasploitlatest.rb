2440599d00d588136a8747594080d3c3a087e7a4
klockw3rk <kw@localhost.localdomain>
2023-03-06 13:01:42 -0500

    yes

--- metasploitlatest.rb
+++ metasploitlatest.rb
@@ -1,35 +1,34 @@
-cask "metasploit" do
-  version "6.3.5,20230225112950"
-  sha256 "41abe5b1ffc1c93ae5459c846f9aa863773744e188c95712bf99e94e974d6583"
+class Surprise < Formula
 
   url "https://osx.metasploit.com/metasploitframework-latest.pkg"
   name "Metasploit Framework"
   desc "Penetration testing framework"
   homepage "https://www.metasploit.com/"
 
+  def install
+      depends_on formula: "nmap"
 
-  depends_on formula: "nmap"
+      pkg "metasploitframework-latest.pkg"
+      binary "/opt/metasploit-framework/bin/msfbinscan"
+      binary "/opt/metasploit-framework/bin/msfconsole"
+      binary "/opt/metasploit-framework/bin/msfd"
+      binary "/opt/metasploit-framework/bin/msfdb"
+      binary "/opt/metasploit-framework/bin/msfelfscan"
+      binary "/opt/metasploit-framework/bin/msfmachscan"
+      binary "/opt/metasploit-framework/bin/msfpescan"
+      binary "/opt/metasploit-framework/bin/msfrop"
+      binary "/opt/metasploit-framework/bin/msfrpc"
+      binary "/opt/metasploit-framework/bin/msfrpcd"
+      binary "/opt/metasploit-framework/bin/msfvenom"
 
-  pkg "metasploitframework-latest.pkg"
-  binary "/opt/metasploit-framework/bin/msfbinscan"
-  binary "/opt/metasploit-framework/bin/msfconsole"
-  binary "/opt/metasploit-framework/bin/msfd"
-  binary "/opt/metasploit-framework/bin/msfdb"
-  binary "/opt/metasploit-framework/bin/msfelfscan"
-  binary "/opt/metasploit-framework/bin/msfmachscan"
-  binary "/opt/metasploit-framework/bin/msfpescan"
-  binary "/opt/metasploit-framework/bin/msfrop"
-  binary "/opt/metasploit-framework/bin/msfrpc"
-  binary "/opt/metasploit-framework/bin/msfrpcd"
-  binary "/opt/metasploit-framework/bin/msfvenom"
+      uninstall script: {
+                  executable: "/opt/metasploit-framework/bin/msfremove",
+                  input:      ["y"],
+                  sudo:       true,
+                },
+                rmdir:  "/opt/metasploit-framework"
 
-  uninstall script: {
-              executable: "/opt/metasploit-framework/bin/msfremove",
-              input:      ["y"],
-              sudo:       true,
-            },
-            rmdir:  "/opt/metasploit-framework"
-
-  zap trash: "~/.msf4"
+      zap trash: "~/.msf4"
+   end
 end
 
